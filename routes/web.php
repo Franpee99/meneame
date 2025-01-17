9a6cb74c1ce8a65671282da70b5aaab75a47e16d
Franpee99 <franjoseperezlozano@gmail.com>
2025-01-17 14:50:02 +0100

    Crear noticias terminado, con logeo incluido

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoticiaController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Noticia;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,8 @@ Route::get('/', function () {
                     'desc')->paginate(8),
     ]);
 })->name('home');
+
+Route::resource('noticias', NoticiaController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
